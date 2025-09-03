bf651fe210a8f937350e8c37e2407513f2bae6f4
harshita-jain512 <harshitasaraf512@gmail.com>
2025-09-04 01:43:43 +0530

    typo mistake

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -5,6 +5,7 @@ module.exports.createReview = async(req,res) =>{
     let listing= await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
     newReview.author = req.user._id;
+    newReview.listing = listing._id;   // ✅ attach listing reference
     listing.reviews.push(newReview);
     await newReview.save();
     await listing.save();
